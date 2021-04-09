980d0421c01764765ce54e5e01d7a1bfbdcd8bda
Vladimir Grishchenko <vgrishchenko@zoon.ru>
2021-04-09 16:28:16 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {SetStateAction, useState} from 'react';
+import Table from 'rc-table';
+import clsx from 'clsx';
+import data from './data';
 import './App.css';
 
+interface Irow {
+    type: string,
+}
+
+const cellRenderer = (selectedRows: String[], selectedLeafs: String[]) => (value: Array<any>, row: Irow) => {
+    const isRowSelected = selectedRows.indexOf(value[0].toString()) !== -1;
+    const isLeafSelected = selectedLeafs.find(item => item === value.toString());
+    return <span data-id={value}
+                 data-row={value[0]}
+                 className={clsx(
+                     'cell',
+                     'cell' + row.type,
+                     isRowSelected && 'row__selected',
+                     isLeafSelected && 'cell__selected',
+                 )}/>
+}
+
+const onCell = (selectedRows: String[],
+                selectedLeafs: String[],
+                setSelectedLeafs: SetStateAction<any>,
+                setSelectedRows: SetStateAction<any>,
+                setClickedData: SetStateAction<any>,
+) => (record: any) => ({
+    onClick(e: any) {
+        setClickedData({name: record.name, type: record.type, position: e.target.dataset.id})
+    },
+    onDoubleClick(e: any) {
+        const rowIndex = e.target.dataset.row;
+        const id = e.target.dataset.id || ''
+        const rowIsSelected = selectedRows.indexOf(rowIndex) === -1;
+        const leafIsSelected = selectedLeafs.indexOf(id) === -1;
+
+        if (record.type === 'Node' || !rowIsSelected && leafIsSelected) {
+            return;
+        }
+
+        if (rowIsSelected) {
+            setSelectedRows([...selectedRows, rowIndex])
+        }
+
+        const index = selectedLeafs.indexOf(id);
+        if (leafIsSelected) {
+            setSelectedLeafs([...selectedLeafs, e.target.dataset.id]);
+            return;
+        }
+        setSelectedLeafs((leafs: String[]) => {
+            const copy = [ ...leafs ];
+            copy.splice(index, 1);
+            return copy;
+        });
+        setSelectedRows((rows: any) => {
+            const copy = [ ...rows ].filter(row => row !== rowIndex);
+            return copy;
+        });
+    }
+});
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedLeafs, setSelectedLeafs] = useState([]);
+    const [selectedRows, setSelectedRows] = useState([]);
+    const [clickedData, setClickedData] = useState({name: '', type: '', position: ''});
+
+    const columns = [
+        {
+            title: 'Tree Name',
+            dataIndex: 'name',
+            key: 'name',
+            width: 100,
+            render: (value: string) => {
+                return <span className='cell cell_first-column'>{value}</span>
+            },
+        },
+        {
+            title: 'Column Header',
+            dataIndex: '',
+            key: 'Column Header',
+            children: new Array(7).fill(0).map((_, index) => ({
+                title: `${index}`,
+                dataIndex: `column_${index}`,
+                key: `column_${index}`,
+                width: 50,
+                render: cellRenderer(selectedRows, selectedLeafs),
+                onCell: onCell(selectedRows, selectedLeafs, setSelectedLeafs, setSelectedRows, setClickedData)
+            }))
+        },
+    ];
+
+    return (
+        <div className='table-wrapper'>
+            <Table columns={columns} data={data}/>
+            <div className='info-wrapper'>
+                <div>
+                    Name: {clickedData.name}
+                </div>
+                <div>
+                    Type: {clickedData.type}
+                </div>
+                <div>
+                    Position(y,x): {clickedData.position}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.ts
@@ -0,0 +1,118 @@
+export default [
+    {
+        name: 'Node 0',
+        type: 'Node',
+        key: 1,
+        column_0: [0, 0],
+        column_1: [0, 1],
+        column_2: [0, 2],
+        column_3: [0, 3],
+        column_4: [0, 4],
+        column_5: [0, 5],
+        column_6: [0, 6],
+        children: [
+            {
+                name: 'Node 1',
+                type: 'Node',
+                column_0: [1, 0],
+                column_1: [1, 1],
+                column_2: [1, 2],
+                column_3: [1, 3],
+                column_4: [1, 4],
+                column_5: [1, 5],
+                column_6: [1, 6],
+                key: 2,
+                children: [
+                    {
+                        name: 'Leaf 1',
+                        type: 'Leaf',
+                        column_0: [2, 0],
+                        column_1: [2, 1],
+                        column_2: [2, 2],
+                        column_3: [2, 3],
+                        column_4: [2, 4],
+                        column_5: [2, 5],
+                        column_6: [2, 6],
+                        key: 9,
+                    },
+                    {
+                        name: 'Leaf 2',
+                        type: 'Leaf',
+                        column_0: [3, 0],
+                        column_1: [3, 1],
+                        column_2: [3, 2],
+                        column_3: [3, 3],
+                        column_4: [3, 4],
+                        column_5: [3, 5],
+                        column_6: [3, 6],
+                        key: 3,
+                    },
+                    {
+                        name: 'Leaf 3',
+                        type: 'Leaf',
+                        column_0: [4, 0],
+                        column_1: [4, 1],
+                        column_2: [4, 2],
+                        column_3: [4, 3],
+                        column_4: [4, 4],
+                        column_5: [4, 5],
+                        column_6: [4, 6],
+                        key: 4,
+                    }
+                ]
+            },
+            {
+                name: 'Node 2',
+                type: 'Node',
+                column_0: [5, 0],
+                column_1: [5, 1],
+                column_2: [5, 2],
+                column_3: [5, 3],
+                column_4: [5, 4],
+                column_5: [5, 5],
+                column_6: [5, 6],
+                key: 5,
+                children: [
+                    {
+                        name: 'Leaf 4',
+                        type: 'Leaf',
+                        column_0: [6, 0],
+                        column_1: [6, 1],
+                        column_2: [6, 2],
+                        column_3: [6, 3],
+                        column_4: [6, 4],
+                        column_5: [6, 5],
+                        column_6: [6, 6],
+                        key: 6,
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        name: 'Node 3',
+        type: 'Node',
+        key: 7,
+        column_0: [7, 0],
+        column_1: [7, 1],
+        column_2: [7, 2],
+        column_3: [7, 3],
+        column_4: [7, 4],
+        column_5: [7, 5],
+        column_6: [7, 6],
+        children: [
+            {
+                name: 'Leaf 5',
+                type: 'Leaf',
+                column_0: [8, 0],
+                column_1: [8, 1],
+                column_2: [8, 2],
+                column_3: [8, 3],
+                column_4: [8, 4],
+                column_5: [8, 5],
+                column_6: [8, 6],
+                key: 8,
+            }
+        ]
+    }
+]
